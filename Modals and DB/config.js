99a78d2636f3a986721aeee485d1c4829290c651
Dhiraj Mahadik <dhirajmahadik9221@gmail.com>
2023-06-01 18:11:45 +0530

    NA

--- Modals and DB/config.js
+++ Modals and DB/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const Connect = () => {
 
-    return mongoose.connect('mongodb+srv://dhirajdemo9221:dhirajdemo%40123@cluster0.10xjk3l.mongodb.net/test')
+    return mongoose.connect('mongodb+srv://dhirajdemo9221:dhiraj%40123@dhirajcloud.i0murtn.mongodb.net/test')
    
 
 }
